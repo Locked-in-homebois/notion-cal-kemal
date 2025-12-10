d29da1f863082a74e54e54347ae37f4f67dd5e33
Kemal Can Bayramoğlu <kemal@192.168.1.108>
2025-12-10 19:03:14 +0300

    pushed

---
+++ app/components/navbar.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link"
+
+export default function TopNavbarTrying() {
+    return (
+      <nav className="flex gap-4 p-4 border-b">
+        <Link href="/">Mail</Link>
+      </nav>
+    )
+  }
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,32 @@
 import Image from "next/image";
+import Link from "next/link";
+import TopNavbarTrying from "./components/navbar";
 
 export default function Home() {
   return (
-    <div className="flex item-center justify-center mt-30"> 
+      
 
+
+    <div className="flex item-center justify-center mt-30">
+      <TopNavbarTrying/>  
       <Image
-
-      src="/calendardrawing.png"
-      width={120}
-      height={60}
-      alt="notsamecalendardrawing" // descrption 8 whatever
-      className="idkwhatwasthis"  // all the css parts for the image flex item center and shi 
-
-        />
-
+        src="/calendardrawing.png"
+        width={120}
+        height={60}
+        alt="notsamecalendardrawing" // descrption 8 whatever
+        className="idkwhatwasthis"  // all the css parts for the image flex item center and shi 
+      />
+      <div>
+        <h1 className="text-lg">  Time is now  </h1>
+      </div>
     </div>
+
+    
+
+
+
+
+
+
   )
 }

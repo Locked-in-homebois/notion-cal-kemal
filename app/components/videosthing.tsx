fe73af8b6e6370dd4d3b3434881862fab6517de7
Kemal Can Bayramoğlu <kemal@192.168.1.101>
2025-12-11 23:15:54 +0300

    monkey king kinkkong

--- app/components/videosthing.tsx
+++ app/components/videosthing.tsx
@@ -3,7 +3,7 @@
 export default function Videofmonkey() {
     return (
         <video
-            className="w-220 h-auto object-cover transform-gpu flex items-center justify-self-center rounded-[12]" autoPlay loop muted playsInline preload="auto"
+            className="w-220 h-auto object-cover transform-gpu flex items-center justify-self-center rounded-[12 mt=6]" autoPlay loop muted playsInline preload="auto"
         >
             <source src="/monkeyvideo.mp4" type="video/mp4" />
         </video>

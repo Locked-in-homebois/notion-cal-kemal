f503cfcc19efd41bd26a1d02dd2abea0c241bc97
Kemal Can Bayramoğlu <kemal@192.168.1.101>
2025-12-11 15:08:58 +0300

    weird world

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function Hero() {
 
     return (<main className="max-w-[380px] ">
-        <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center justify-center mt-15">
             <Image
                 src="/calendardrawing.png"
                 alt="notsamecalendardrawing"
@@ -12,9 +12,9 @@ export default function Hero() {
             />
 
             <h1 className="text-[20px] font-semibold mt-1">Notion Calendar</h1>
-            <h2 className="font-bold text-[70px] mt-3 ">It's time</h2>
-            <p className="text-[20px] font-semibold mt-4 ">All of your commitments, now in one place. Meet the beautifully designed, fully integrated calendar for your work and life.</p>
-            <Link className="bg-black text-white rounded-[8] px-4 py-2" href="/">Get Notion Callendar Free</Link>
+            <h2 className="font-bold text-[70px] mt-3 ">It's time.</h2>
+            <p className="text-[20px] mt-4 ">All of your commitments, now in one place. Meet the beautifully designed, fully integrated calendar for your work and life.</p>
+            <Link className="bg-black text-white rounded-[8] px-4 py-2 mt-5" href="/">Get Notion Callendar Free</Link>
 
 
         </div>
--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,16 +1,33 @@
 import Link from "next/link"
-
+import Image from "next/image"
 export default function TopNavbarTrying() {
   return (
-    <nav className="sticky items-center justify-center flex gap-6 p-4 border-b">
+
+    <nav className="sticky font-semibold items-center justify-center flex gap-8 p-4 border-b">
+      <Link href="/">
+        <Image
+                  src="/notionsymbol.png"
+                  width={60}
+                  height={30}
+                  alt="topleftsymbol" // descrption 8 whatever
+                  className="absolute left-4"  // all the css parts for the image flex item center and shi         
+                />
+      </Link>
       <Link href="/">Mail</Link>
       <Link href="/">Calendar</Link>
       <Link href="/">AI</Link>
       <Link href="/">Enterprise</Link>
       <Link href="/">Pricing</Link>
-     <Link href="/">Explore</Link>
-     <Link href="/">Request a Demo</Link>
-
+      <Link href="/">Explore</Link>
+      <Link href="/">Request a Demo</Link>
+      <div className="absolute right-4 ">
+        <Link className="mr-3" href="/">Log in</Link>
+        <Link className="bg-black text-white rounded-[8] px-4 py-2 " href="/">Get Notion Calendar Free</Link>
+      </div>
     </nav>
+
+
+
+
   )
 }

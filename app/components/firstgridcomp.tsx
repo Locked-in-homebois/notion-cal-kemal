5af26cd9ca160c333c90e3b51e4a4b279e134f28
Kemal Can Bayramoğlu <kemal@192.168.1.101>
2025-12-12 23:37:23 +0300

    pushkin

--- app/components/firstgridcomp.tsx
+++ app/components/firstgridcomp.tsx
@@ -50,48 +50,51 @@ export default function gridcardfirst() {
 
                             />    </div>
                     </div>
+                    <div className="bg-gray-100 rounded-[12px] w-full h-full flex flex-col">
+                        <div className=" px-10 pt-5"        >
 
-                    <div className="bg-gray-100 rounded-[12px] w-full h-full px-10 pt-5 ">
-
-                        <Image
-                            src="/cardone/cardone3icon.png"
-                            alt="card1icon3"
-                            width={50}
-                            height={50}
-                        />
-                        <h1 className="font-bold text-[20px]"> Modern design </h1>
-                        <p>Use command menu and shortcuts for efficient workflows.</p>
-                        <div className="flex-1 relative w-full min-h-0 mt-5">
+                            <Image
+                                src="/cardone/cardone3icon.png"
+                                alt="card1icon3"
+                                width={50}
+                                height={50}
+                            />
+                            <h1 className="font-bold text-[20px]"> Modern design </h1>
+                            <p>Use command menu and shortcuts for efficient workflows.</p>
+                        </div>
+                        <div className="mt-auto px-6">
                             <Image
                                 src="/cardone/cardone3.png"
                                 alt="cardone3"
                                 width={500}
                                 height={500}
+                                className="block w-full h-auto object-contain"
                             />
                         </div>
                     </div>
 
 
-                    <div className="bg-gray-100 rounded-[12px] w-full h-full px-10 pt-5">
+                    <div className="bg-gray-100 rounded-[12px] w-full h-full px-10 pt-5 flex flex-col">
 
-
-                        <Image
-                            src="/cardone/cardone4icon.png"
-                            alt="card1icon4"
-                            width={50}
-                            height={50}
-                        />
-                        <h1 className="font-bold text-[20px]"> Available in 12 languages </h1>
-                        <p>Supports English, Japanese, Korean, French, German, Spanish, Portuguese, Danish, Dutch, Finnish, Norwegian, and Swedish… with more on the way!  </p>
-
-                        <Image className="py-8"
-                            src="/cardone/cardone4.png"
-                            alt="cardone4"
-                            width={400}
-                            height={400}
-                        />
+                        <div className="">
+                            <Image
+                                src="/cardone/cardone4icon.png"
+                                alt="card1icon4"
+                                width={50}
+                                height={50}
+                            />
+                            <h1 className="font-bold text-[20px]"> Available in 12 languages </h1>
+                            <p>Supports English, Japanese, Korean, French, German, Spanish, Portuguese, Danish, Dutch, Finnish, Norwegian, and Swedish… with more on the way!  </p>
+                        </div>
+                        <div className="mt-auto px-6">
+                            <Image className="block w-full h-auto object-contain"
+                                src="/cardone/cardone4.png"
+                                alt="cardone4"
+                                width={400}
+                                height={400}
+                            />
+                        </div>
                     </div>
-
                 </div>
             </div>
         </main >

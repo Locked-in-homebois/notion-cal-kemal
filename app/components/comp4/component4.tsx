1ebe01448575a03d69b1906eaa9e6913ded0f996
Kemal Can Bayramoğlu <kemal@192.168.1.108>
2025-12-14 00:28:30 +0300

    let me sleep now yarraki

--- app/components/comp4/component4.tsx
+++ app/components/comp4/component4.tsx
@@ -1,22 +1,20 @@
-import Image from "next/image"
-import Link from "next/link"
-
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Lastcomp() {
-    return
-    (
-        <main>
-            <h1 className="font-bold ">     Get Notion Calendar for free. </h1>
-            <p>      Unlock a better way to manage your time and life.       </p>
-            <Link className="text-blue-600 " href="Get Notion Calendar Free -->"></Link>
+    return (
+        <main className="">
 
-            <div className="flex justify-center">           </div>
+            <div className="flex flex-col justify-center items-center">
+                <h1 className="font-bold text-[40px]"> Get Notion Calendar for free. </h1>
+                <p className="mt=10 text-[20px]"> Unlock a better way to manage your time and life. </p>
 
-
-
-
-
-
+                <Link
+                    className="text-blue-600 "
+                    href="/"
+                >  Get Notion Calendar Free ! </Link>
+            </div>
+            <div className=""> </div>
         </main>
     )
 }

5d4da634530337688c7a169ef19ad01d610d2ecb
Kemal Can Bayramoğlu <kemal@192.168.1.108>
2025-12-15 15:33:01 +0300

    still working on the last comp amk

--- app/components/comp4/component4.tsx
+++ app/components/comp4/component4.tsx
@@ -3,40 +3,32 @@ import Link from "next/link";
 
 export default function Lastcomp() {
     return (
-        <main className="flex flex-col justify-center items-center">
-            <div className="flex flex-col">
-                <h1 className="font-bold text-[40px]"> Get Notion Calendar for free. </h1>
-                <p className="mt-10 text-[20px]"> Unlock a better way to manage your time and life. </p>
-                <Link
-                    className="text-blue-600 mt-5 "
-                    href="/"
-                >  Get Notion Calendar Free ! </Link>
-            </div>
-
-
-            <div className="w-[1000px] h-[500px] bg-gray-100 flex">
-                <div className="grid grid-rows-2 gap-5 ">
-                    <div className="bg-gray-100 rounded-xl w-full h-full flex flex-col">
-                        <div>
-                            <h1 className="font-bold text-2xl p-5">Desktop app</h1>
-                            <Image
-                                src="/comp4image1.png"
-                                alt="comp4-1"
-                                height={300}
-                                width={300}
-                            />
-                        </div>
-
-                        <div>
-                            <h1 className="font-bold text-2xl p-5"> Mobile App</h1>
-                            <Image
-                                src="/comp4image2.png"
-                                alt="comp4-2"
-                                height={300}
-                                width={300}
-                            />
-                        </div>
-                    </div>
+        <main className="flex flex-col items-center text-center">
+            <h1 className="font-bold text-[40px]"> Get Notion Calendar for free. </h1>
+            <p className="text-[20px]"> Unlock a better way to manage your time and life. </p>
+            <Link
+                className="text-blue-600 mt-4 "
+                href="/"
+            >  Get Notion Calendar Free ! </Link>
+            <div className="max-w-[1000px] w-full mt-10 grid grid-cols-2 gap-5 ">
+                <div className="bg-gray-100 flex flex-col items-center rounded-2xl ">
+                    <h1 className="font-bold text-2xl p-5">Desktop app</h1>
+                    <Image
+                        src="/comp4image1.png"
+                        alt="comp4-1"
+                        height={400}
+                        width={400}
+                    // sadece linkleri eklemek falan kaldi sakso bir de paragrafi basa sabitlemek
+                    />
+                </div>
+                <div className="bg-gray-100 rounded-xl flex flex-col items-center ">
+                    <h1 className="font-bold text-2xl flex p-5 "> Mobile App</h1>
+                    <Image
+                        src="/comp4image2.png"
+                        alt="comp4-2"
+                        height={300}
+                        width={300}
+                    />
                 </div>
             </div>
         </main >

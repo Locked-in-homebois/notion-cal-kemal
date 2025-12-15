d23362f71936ebc35fd1b0c0f669a6f538c10419
Kemal Can Bayramoğlu <kemal@192.168.1.101>
2025-12-15 14:36:03 +0300

    clean unlike last comp everything clean

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -3,17 +3,17 @@ import Link from "next/link"
 export default function Hero() {
 
     return (<main >
-        <div className="flex flex-col items-center justify-center mt-15">
+        <div className="flex flex-col items-center justify-center mt-16">
             <Image
                 src="/calendardrawing.png"
                 alt="notsamecalendardrawing"
                 width={120}
                 height={60}
             />
-            <h1 className="text-[20px] font-semibold mt-1">Notion Calendar</h1>
-            <h2 className="font-bold text-[70px] mt-3 ">It's time.</h2>
-            <p className="text-[20px] mt-4 max-w-[380px] font-[515] text-center">All of your commitments, now in one place. Meet the beautifully designed, fully integrated calendar for your work and life.</p>
-            <Link className="bg-black text-white rounded-[8] px-4 py-2 mt-5" href="/">Get Notion Callendar Free</Link>
+            <h1 className="text-[20px] font-semibold ">Notion Calendar</h1>
+            <h2 className="font-bold text-[70px]  ">It's time.</h2>
+            <p className="text-[20px] md:max-w-[380px] font-[515] max-w-[250px] text-center">All of your commitments, now in one place. Meet the beautifully designed, fully integrated calendar for your work and life.</p>
+            <Link className="bg-black text-white rounded-lg px-4 py-2 mt-6" href="/">Get Notion Callendar Free</Link>
         </div>
     </main>
     )

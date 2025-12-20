10d4712e556fff5d6c037647e2f01fba71cd69b4
Kemal can bayramoğlu <bayramoglukemalcan@gmail.com>
2025-12-20 17:07:26 +0300

    FINISHEDAMKKKKK

--- app/components/faq/faqlist.tsx
+++ app/components/faq/faqlist.tsx
@@ -7,26 +7,28 @@ interface thePropsProps {
     quesions: string
     answeres: string
     coloredolores: string
+    textcolores: string
 }
 
-const looperpropertiepart: React.FC<thePropsProps> = ({ quesions, answeres, coloredolores }) => {
+const looperpropertiepart: React.FC<thePropsProps> = ({ quesions, answeres, coloredolores, textcolores }) => {
 
     const [isOpen, setIsOpen] = useState(false);
     const toggleMenu = () => setIsOpen((prev) => !prev);
     return (
 
-        <div className={`flex flex-col container transition-colors bg-${coloredolores} justify-center max-w-[1000px]`} >
+        <div className={`flex flex-col container px-4 py-2 bg-${coloredolores} text-${textcolores} border-b ring-offset-1 border-white  max-w-[1000px]`} >
 
-            <button onClick={toggleMenu} className="flex justify-end" >
-                <h1 className={`text-lg font-medium `}>{quesions}</h1>
+            <button onClick={toggleMenu} className=" flex justify-between" >
+                <h1 className={` text-xl  font-medium `}>{quesions}</h1>
                 <Plus className={`duration-300  transition-transform ${isOpen ? "rotate-45" : "rotate-0"}`} />
             </button>
 
             {isOpen && (
 
-                <p className={``}>{answeres}</p>
+                <p >{answeres}</p>
             )}
         </div>
+
         // : React.FC<QAItemProps> = ({ question, answer }) => {
     )
 }
--- app/components/faq/realFAQ.tsx
+++ app/components/faq/realFAQ.tsx
@@ -4,20 +4,24 @@ const itemlisty = [{
 
     quesions: "Which calendar provider(s) is Notion Calendar compatible with? "
     , answeres: "Currently, Notion Calendar integrates and syncs with Google Calendar accounts and Apple iCloud-synced Calendars. Adding support for other calendar providers such as Outlook is on our roadmap."
-    , coloredolores: "red"
+    , coloredolores: "black"
+    , textcolores: "white"
 
 },
 
 {
     quesions: "Is Notion Calendar available on mobile devices?"
     , answeres: "Yes, Notion Calendar is available for iPhone and Android devices. We know some users are looking for an optimized version for tablet devices, such as iPad, and are working to make Notion Calendar the best experience on all platforms."
-    , coloredolores: "red"
+    , coloredolores: "red-500"
+    , textcolores: "black"
 },
 {
     quesions: "Does Notion Calendar bring Google Calendar sync to Notion?"
     , answeres: "Notion Calendar allows you to view your Notion database items alongside your Google Calendar events. This offers a streamlined way to see project timelines and task due dates alongside other scheduled events. However, importing Google Calendar events directly into a Notion database is not yet possible. We’re always exploring new ways for Notion and calendars to interact. Expect exciting developments soon!"
-    , coloredolores: "yellow"
-}
+    , coloredolores: "yellow-500"
+    , textcolores: "black"
+},
+
 
 
 ]
@@ -27,23 +31,25 @@ export default function RealFakin() {
 
     return (
 
-        <div className="flex  flex-col items-center max-w-[1000px]  mt-20 ">
-            <h1 className=" text-5xl">Questions & Answers</h1>
+        <div className="flex flex-col items-center mt-20 ">
 
-            {itemlisty.map((dalgalar, index) => (
-                <Looperpropertiepart
-                    key={index}
-                    quesions={dalgalar.quesions}
-                    answeres={dalgalar.answeres}
-                    coloredolores={dalgalar.coloredolores}
-                />
+            <div className="max-w-[1000px] w-full">
+                <h1 className="mb-5 text-5xl">Questions & Answers</h1>
+
+                {itemlisty.map((dalgalar, index) => (
+                    <Looperpropertiepart
+                        key={index}
+                        quesions={dalgalar.quesions}
+                        answeres={dalgalar.answeres}
+                        coloredolores={dalgalar.coloredolores}
+                        textcolores={dalgalar.textcolores}
+                    />
+
+                ))
 
 
-            ))
-
-
-            }
-
+                }
+            </div>
 
         </div>
 

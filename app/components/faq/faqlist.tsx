5f6fa0476f08fd071d0635ac8fba890864d019bc
Kemal can bayramoğlu <bayramoglukemalcan@gmail.com>
2025-12-20 15:47:06 +0300

    xd

--- app/components/faq/faqlist.tsx
+++ app/components/faq/faqlist.tsx
@@ -8,33 +8,25 @@ interface thePropsProps {
     answeres: string
 }
 
-const [isOpen, setIsOpen] = useState(false)
-const toggleMenu = () => setIsOpen((prev) => !prev);
-
-
 const looperpropertiepart: React.FC<thePropsProps> = ({ quesions, answeres }) => {
 
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleMenu = () => setIsOpen((prev) => !prev);
     return (
 
-        <div className="flex flex-col" >
+        <div className="flex flex-col container justify-center max-w-[1000px]" >
 
-            <button onClick={toggleMenu} className="" >
-                <h1>{quesions}</h1>
-                <Plus className={`duration-300 transition-transform ${isOpen ? "rotate-45" : "rotate-0"}`} />
+            <button onClick={toggleMenu} className="flex justify-end" >
+                <h1 className="text-lg font-medium">{quesions}</h1>
+                <Plus className={`duration-300  transition-transform ${isOpen ? "rotate-45" : "rotate-0"}`} />
             </button>
 
-            isOpen && (
+            {isOpen && (
 
-            <p>{answeres}</p>
-            )
-
+                <p className="">{answeres}</p>
+            )}
         </div>
-
         // : React.FC<QAItemProps> = ({ question, answer }) => {
-
-
-
-
     )
 }
 export default looperpropertiepart;
--- app/components/faq/realFAQ.tsx
+++ app/components/faq/realFAQ.tsx
@@ -1,16 +1,46 @@
-import looperpropertiepart from "./faqlist"
+import Looperpropertiepart from "./faqlist"
+
+const itemlisty = [{
+
+    quesions: "Which calendar provider(s) is Notion Calendar compatible with? "
+    , answeres: "Currently, Notion Calendar integrates and syncs with Google Calendar accounts and Apple iCloud-synced Calendars. Adding support for other calendar providers such as Outlook is on our roadmap."
+
+},
+
+{
+    quesions: "Is Notion Calendar available on mobile devices?"
+    , answeres: "Yes, Notion Calendar is available for iPhone and Android devices. We know some users are looking for an optimized version for tablet devices, such as iPad, and are working to make Notion Calendar the best experience on all platforms."
+
+},
+{
+    quesions: "Does Notion Calendar bring Google Calendar sync to Notion?"
+    , answeres: "Notion Calendar allows you to view your Notion database items alongside your Google Calendar events. This offers a streamlined way to see project timelines and task due dates alongside other scheduled events. However, importing Google Calendar events directly into a Notion database is not yet possible. We’re always exploring new ways for Notion and calendars to interact. Expect exciting developments soon!"
+
+}
 
 
-
+]
+// 
 export default function RealFakin() {
 
 
     return (
 
-        <div>
-            <h1>Questions & Answers</h1>
+        <div className="flex  flex-col items-center max-w-[1000px]  mt-20 ">
+            <h1 className=" text-5xl">Questions & Answers</h1>
+
+            {itemlisty.map((dalgalar, index) => (
+                <Looperpropertiepart
+                    key={index}
+                    quesions={dalgalar.quesions}
+                    answeres={dalgalar.answeres}
+                />
 
 
+            ))
+
+
+            }
 
 
         </div>
